04aa3fb169708d48c88e9176d925952d91c50080
simu88 <105586812+simu88@users.noreply.github.com>
2025-05-07 17:54:20 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 4.82 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int left, int right) {
+       
+        
+       
+        int sum=0;
+        for(int i=left;i<=right;i++){
+    
+            int count=0;
+            for(int j=1;j<=i;j++){
+                if(i%j==0){
+                    count++;
+                }
+            }
+            
+            if(count%2==0){
+                sum+=i;
+            }else{
+                sum-=i;
+            }
+        }
+        
+        return sum;
+    }
+}
